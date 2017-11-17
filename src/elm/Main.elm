9ba30b0fe2f16f6c1b1511131b0f597d1ffc0c81
Jonathan D. Baker <jonathan@ardent.tech>
2017-11-17 15:31:40 -0700

    initial data modeling

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,17 +1,49 @@
 module Main exposing (..)
 
 import Html exposing (Html, div)
+import Date exposing (Date)
 
 
-type alias Model = {}
+type VictimType = Injured | Killed
+
+
+type alias Filter = {
+  victimTypes : List VictimType,
+  years : List Int  -- @todo limit this to 2014-2017
+}
+
+
+type State = CO | TX
+
+
+type alias Record = {
+  date : Date,
+  injured : Int,
+  killed : Int,
+  state : State
+}
+
+
+type alias Model = {
+  filter : Filter,
+  records : List Record
+}
+
 
 init : ( Model, Cmd Msg )
-init = ( {}, Cmd.none )
+init = ({
+    filter = { victimTypes = [ Killed ], years = [ 2017 ] },
+    records = []
+  }, Cmd.none )
+
+
+--
 
 
 type Msg = NoOp
 
 
+-- @todo if updating model.selectedYears only allow 2014-2017
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
