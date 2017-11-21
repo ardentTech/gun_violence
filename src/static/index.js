e7ddc5a969b32db2369abad94c4e7ccc1bbf6288
Jonathan D. Baker <jonathan@ardent.tech>
2017-11-20 20:19:38 -0700

    basic d3 chloropeth

--- src/static/index.js
+++ src/static/index.js
@@ -1,14 +1,94 @@
-// pull in desired CSS/SASS files
-require( './styles/main.scss' );
-var $ = jQuery = require( '../../node_modules/jquery/dist/jquery.js' );           // <--- remove if jQuery not needed
-require( '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js' );   // <--- remove if Bootstrap's JS not needed 
-
 // inject bundled Elm app into div#main
 var Elm = require( '../elm/Main' ),
     app = Elm.Main.embed( document.getElementById( 'main' ) );
 
-console.log(app);
-
 app.ports.newState.subscribe(function(payload) {
-    console.log(payload);
+    update(payload);
 });
+
+var states = {
+    "01": "Alabama",
+    "02": "Alaska",
+    "04": "Arizona",
+    "05": "Arkansas",
+    "06": "California",
+    "08": "Colorado",
+    "09": "Connecticut",
+    "10": "Delaware",
+    "10": "District of Columbia",
+    "12": "Florida",
+    "13": "Georgia",
+    "15": "Hawaii",
+    "16": "Idaho",
+    "17": "Illinois",
+    "18": "Indiana",
+    "19": "Iowa",
+    "20": "Kansas",
+    "21": "Kentucky",
+    "22": "Louisiana",
+    "23": "Maine",
+    "24": "Maryland",
+    "25": "Massachusetts",
+    "26": "Michigan",
+    "27": "Minnesota",
+    "28": "Mississippi",
+    "29": "Missouri",
+    "30": "Montana",
+    "31": "Nebraska",
+    "32": "Nevada",
+    "33": "New Hampshire",
+    "34": "New Jersey",
+    "35": "New Mexico",
+    "36": "New York",
+    "37": "North Carolina",
+    "38": "North Dakota",
+    "39": "Ohio",
+    "40": "Oklahoma",
+    "41": "Oregon",
+    "42": "Pennsylvania",
+    "44": "Rhode Island",
+    "45": "South Carolina",
+    "46": "South Dakota",
+    "47": "Tennessee",
+    "48": "Texas",
+    "49": "Utah",
+    "50": "Vermont",
+    "51": "Virginia",
+    "53": "Washington",
+    "54": "West Virginia",
+    "55": "Wisconsin",
+    "56": "Wyoming",
+};
+
+var svg = d3.select("svg"),
+    path = d3.geoPath();
+
+function update(data) {
+    console.log("update()");
+}
+
+function render() {
+   d3.queue()
+       .defer(d3.json, "https://d3js.org/us-10m.v1.json")
+       .await(ready);
+}
+
+function ready(error, us, stateData) {
+    if (error) throw error;
+
+    svg.append("g")
+        .attr("class", "states")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
+        .enter().append("path")
+            .attr("d", path);
+//            .append("svg:title").text(function(d) { return states[d.id]; });
+
+    // @todo what is this for?
+    svg.append("path")
+        .attr("class", "state-borders")
+        .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) {
+            return a !== b; })));
+}
+
+render();
