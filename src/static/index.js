b737c4a9a3352da051488cfb697702158012ea17
Jonathan D. Baker <jonathan@ardent.tech>
2017-12-06 14:20:51 -0700

    datastore batch loading

--- src/static/index.js
+++ src/static/index.js
@@ -3,21 +3,53 @@ const Elm = require( "../elm/Main" );
 
 class DataStore {
 
-    // @todo should this manage all access?
-    load(ready) {
-        const sources = [[d3.json, "static/data/us-10m.v1.json"], [d3.csv, "static/data/stats.csv"]];
-        let queue = d3.queue();
-        sources.map((d) => { queue.defer(d[0], d[1]); });
-        queue.awaitAll(ready);
+    constructor() {
+        this.data = {};
+        this.sourcesLoaded = 0;
+
+        this._loadedCallback = null;
+        this._sources = [];
+    }
+
+    set loadedCallback(func) { this._loadedCallback = func; }
+
+    set sources(s) { this._sources = s; }
+    
+    load() {
+        this._sources.forEach((s) => s.load((data) => this.loaded(data, s.name)));
+    }
+
+    loaded(data, name) {
+        this.data[name] = data;
+        this.sourcesLoaded++;
+        if (this.sourcesLoaded == this._sources.length) this._loadedCallback(data);
+    }
+}
+
+
+class DataSource {
+
+    constructor (name, path, formatter) {
+        this.formatter = formatter;
+        this.name = name;
+        this.path = path;
+    }
+
+    load(callback) {
+        d3.queue().defer(this.formatter, this.path).await((error, data) => {
+            if (error) throw error;
+            this.data = data;
+            callback(data);
+        });
     }
 }
 
 
 class Vis {
 
-    parseData(error, topo, stats) {
-        if (error) throw error;
+    parseData(data) {
         console.log("Vis.parseData");
+        console.log(data);
     }
 
     update(state) {
@@ -28,32 +60,33 @@ class Vis {
 
 class App {
     constructor(elmSelector) {
+        this.dataStore = new DataStore();
         this.elmSelector = elmSelector;
         this.elmApp = Elm.Main.embed(document.getElementById(this.elmSelector));
         this.vis = new Vis();
-        this.dataStore = new DataStore();
+    }
+
+    dataSources() {
+        return [
+            new DataSource("us.states", "static/data/us-10m.v1.json", d3.json),
+            new DataSource("gun.violence", "static/data/stats.csv", d3.csv)
+        ];
+    }
+
+    loadData() {
+        this.dataStore.sources = this.dataSources();
+        this.dataStore.loadedCallback = this.vis.parseData;
+        this.dataStore.load();
     }
 
     main() {
-        this.dataStore.load(this.vis.parseData);
-        this.elmApp.ports.newState.subscribe(this.newUIState);
-
-//        var years = new Set();
-//        for (var state in data) {
-//            for (var year in data[state].stats) {
-//                years.add(year);
-//            } 
-//        }
-//
-//        app.ports.categories.send(["Incidents", "Injured", "Killed", "Victims"]);
-//        app.ports.years.send(Array.from(years)
-//            .sort(function(a, b) {return b - a; })
-//            .map(function(y) { return parseInt(y); }));
+        this.loadData();
+//        this.elmApp.ports.newState.subscribe(this.newUIState);
     }
 
-    newUIState(state) {
-        this.vis.update(state);
-    }
+//    newUIState(state) {
+//        this.vis.update(state);
+//    }
 }
 
 
